31c6ee9f64364f0a584cbf609e9c72609e0a4742
ncard05 <ncardenas@byu.edu>
2025-07-05 16:18:16 -0500

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        bool correctGuess = false;
+
+        while (correctGuess == false)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            int guess = int.Parse(input);
+
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+                correctGuess = true;
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("higher");
+            }
+        } 
     }
 }
